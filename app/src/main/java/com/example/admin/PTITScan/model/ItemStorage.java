f162ddfb98e4120e05f7e7a46f1e199fcaebb5f8
mtuan <tuanxsokoh@gmail.com>
2023-10-27 12:52:03 +0700

    docs: update readme

---
+++ app/src/main/java/com/example/admin/PTITScan/model/ItemStorage.java
@@ -0,0 +1,14 @@
+package com.example.admin.PTITScan.model;
+
+import java.util.Date;
+
+public class ItemStorage {
+    Item item;
+    Storage storage;
+    int quantity;
+    Date date;
+
+    public ItemStorage() {
+
+    }
+}
---
+++ app/src/main/java/com/example/admin/PTITScan/model/UserStorage.java
@@ -0,0 +1,10 @@
+package com.example.admin.PTITScan.model;
+
+public class UserStorage {
+    User user;
+    Storage storage;
+
+    public UserStorage() {
+
+    }
+}
